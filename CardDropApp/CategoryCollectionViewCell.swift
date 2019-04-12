2372eacfcfbade2bd3888ca834b2db97514d70d5
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-11 18:23:05 -0700

    - Loaded Category Data

---
+++ CardDropApp/CategoryCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryCollectionViewCell.swift
+//  CardDropApp
+//
+//  Created by Prudhvi Gadiraju on 4/11/19.
+//  Copyright © 2019 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
--- CardDropApp/OverviewController.swift
+++ CardDropApp/OverviewController.swift
@@ -9,9 +9,46 @@
 import UIKit
 
 class OverviewController: UICollectionViewController {
+    
+    let categoryDataRequest = DataRequest<Category>(dataSource: "Categories")
+    var categoryData = [Category]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadData()
+    }
+    
+    func loadData() {
+        categoryDataRequest.getData { [weak self] (dataResult) in
+            switch dataResult {
+            case .failure:
+                print("Couldn't Load Categories")
+            case .success(let categories):
+                self?.categoryData = categories
+            }
+        }
+    }
+}
+
+extension OverviewController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categoryData.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CategoryCollectionViewCell
         
+        let category = categoryData[indexPath.row]
+        let image = category.categoryImageName
+        
+        cell.image.image = UIImage(named: image)
+        cell.label.text = category.categoryName
+        
+        return cell
     }
 }
